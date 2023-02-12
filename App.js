66d9ce75838649dba2728eea06338a5e3558cffa
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2023-02-12 20:19:55 +0200

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image, StyleSheet, TextInput, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [keyword, setKeyword] = useState('');
+
+  useEffect(() => {
+    const getRecipes = () => {
+      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+        .then(response => response.json())
+        .then(data => setRecipes(data.meals))
+        .catch(error => {
+          console.error('Error fetching recipes: ', error);
+        });
+    };
+
+    getRecipes();
+  }, [keyword]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={recipes}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.recipe}>
+            <Image style={styles.thumbnail} source={{ uri: item.strMealThumb }} />
+            <Text style={styles.title}>{item.strMeal}</Text>
+          </View>
+        )}
+      />
+      <TextInput
+        style={styles.input}
+        value={keyword}
+        onChangeText={text => setKeyword(text)}
+        placeholder="Enter ingredient"
+      />
+      <Button title="Search" onPress={() => setKeyword(keyword)} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 10,
+    marginTop: 50
+  },
+  input: {
+    height: 40,
+    marginVertical: 10,
+    padding: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  recipe: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  thumbnail: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 10,
   },
 });
+
+export default App;
